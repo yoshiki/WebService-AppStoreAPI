c3d5cf8f09254485b6a7621eed4000a89376a9cd
clouder(Yoshiki Kurihara) <clouder@gmail.com>
2011-01-19 14:50:15 +0900

    initial commit

---
+++ lib/WebService/AppStoreAPI.pm
@@ -0,0 +1,32 @@
+package WebService::AppStoreAPI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::AppStoreAPI -
+
+=head1 SYNOPSIS
+
+  use WebService::AppStoreAPI;
+
+=head1 DESCRIPTION
+
+WebService::AppStoreAPI is
+
+=head1 AUTHOR
+
+Yoshiki Kurihara E<lt>kurihara at cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
